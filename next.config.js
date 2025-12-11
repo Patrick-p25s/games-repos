35a47236c48dbcb78f3602274bf4992c41fbe3d6
pa nom <nomp6836@gmail.com>
2025-12-11 05:37:14 +0000

    Make all of the change now and make sure that it work correctly

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
+// Fichier de configuration pour Next.js.
+// Il vous permet de personnaliser le comportement de votre application Next.js.
 const nextConfig = {
+  // Le mode strict de React aide à identifier les problèmes potentiels dans l'application.
   reactStrictMode: true,
+  // Active la minification SWC pour des builds plus rapides.
   swcMinify: true,
   typescript: {
+    // Ignore les erreurs de build TypeScript. Utile en développement, mais à utiliser avec prudence.
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Ignore les erreurs ESLint pendant le build.
     ignoreDuringBuilds: true,
   },
+  // Configure les domaines d'images autorisés pour le composant Next.js Image.
+  // Cela améliore la sécurité et les performances.
   images: {
     remotePatterns: [
       {
@@ -29,14 +37,6 @@ const nextConfig = {
         pathname: '/**',
       }
     ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/genkit/:slug*',
-        destination: '/api/genkit/:slug*',
-      },
-    ];
   },
 };
 
